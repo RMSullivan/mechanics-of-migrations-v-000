e00da4148e813916c3f48f8813eaecee259671ad
Rita Sullivan <ritarojas@mac.com>
2018-08-20 00:16:09 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -9,10 +9,13 @@ require 'bundler/setup'
 Bundler.require
 
 
-ActiveRecord::Base.establish_connection(
+# put the code to connect to the database here
+connection = ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artists.sqlite"
 )
 
+
+ 
 
 require_relative "../artist.rb"
