40903779107eef4feb1dfda4c4d9e76a6b23ccf0
Rita Sullivan <ritarojas@mac.com>
2018-08-20 00:16:59 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,19 +1,10 @@
-require 'sinatra/activerecord/rake'
-
 class CreateArtists < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-
   def change
       create_table :artists do |t|
-        t.string :name
-        t.string :genre
-        t.integer :age
-        t.string :hometown
-    end
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
   end
-
+  end
 end
